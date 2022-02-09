c2a990659eb1d0c829b89ec3e73644f86e48739c
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-02-10 08:49:51 +0900

    add header

---
+++ src/organisms/layout/Header.tsx
@@ -0,0 +1,60 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
+import { memo, VFC } from "react";
+
+export const Header: VFC = memo(() => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <>
+            <Flex
+                as="nav"
+                bg="teal.500"
+                color="gray.50"
+                align="center"
+                justify="space-between"
+                padding={{ base: 3, md: 5 }}
+            >
+                <Flex
+                    align="center"
+                    as="a"
+                    mr={8}
+                    _hover={{ cursor: "pointer" }}
+                >
+                    <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+                        UserManagementApp
+                    </Heading>
+                </Flex>
+                <Flex
+                    align="center"
+                    fontSize="sm"
+                    flexGrow={2}
+                    display={{ base: "none", md: "flex" }}
+                >
+                    <Box pr={4}>
+                        <Link>Users</Link>
+                    </Box>
+                    <Link>Settings</Link>
+                </Flex>
+                <IconButton
+                    aria-label="menu"
+                    icon={<HamburgerIcon />}
+                    size="sm"
+                    variant="unstyled"
+                    display={{ base: "block", md: "none" }}
+                    onClick={onOpen}
+                />
+            </Flex>
+            <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+                <DrawerOverlay>
+                    <DrawerContent>
+                        <DrawerBody p={0} bg="gray.100">
+                            <Button w="100%">Top</Button>
+                            <Button w="100%">Users</Button>
+                            <Button w="100%">Settings</Button>
+                        </DrawerBody>
+                    </DrawerContent>
+                </DrawerOverlay>
+            </Drawer>
+        </>
+    );
+});
